4e10b101302cd94335a5bc33b7e374ce9259364b
acandraa91-ac <acandraa91@gmail.com>
2026-05-04 13:28:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const CONFIG = {
     // Atur waktu pengumuman di sini
     // Format: "YYYY-MM-DDTHH:MM:SS"
     // Contoh untuk tanggal 3 Mei 2026 jam 10 pagi: "2026-05-03T10:00:00"
-    TARGET_WAKTU: "2026-05-04T05:01:00"
+    TARGET_WAKTU: "2026-05-04T05:17:00:00"
 };
